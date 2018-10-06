752186670e948d1e49929d379eae943e80223b74
MaximilanL <maxpuzatik@gmail.com>
2018-10-07 02:28:01 +0300

    commit message

---
+++ src/EventSubscriber/RegistrationNotifySubscriber.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use App\Entity\User;
+use App\Events;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\EventDispatcher\GenericEvent;
+
+/**
+ * Sending a welcome letter every time a user is created
+ *
+ */
+class RegistrationNotifySubscriber implements EventSubscriberInterface
+{
+    private $mailer;
+    private $sender;
+
+    public function __construct(\Swift_Mailer $mailer, $sender)
+    {
+        $this->mailer = $mailer;
+        $this->sender = $sender;
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            Events::USER_REGISTERED => 'onUserRegistrated',
+        ];
+    }
+
+    public function onUserRegistrated(GenericEvent $event): void
+    {
+        /** @var User $user */
+        $user = $event->getSubject();
+
+        $subject = "Welcome
+";
+        $body = "Welcome to quiz!";
+
+        $message = (new \Swift_Message())
+            ->setSubject($subject)
+            ->setTo($user->getEmail())
+            ->setFrom($this->sender)
+            ->setBody($body, 'text/html')
+        ;
+
+        $this->mailer->send($message);
+    }
+}
